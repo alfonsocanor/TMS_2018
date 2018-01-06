a6d63c4c0bb4e78f2c9a2ca3afd414bd89f30400
alfonsocano <lalfonsocanor@gmail.com>
2018-01-06 20:11:56 -0300

    Redo_06012018
    I build some line of code that allows me to get all the information from the matrix

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from geopy.distance import vincenty
 from urllib.request import urlopen
 import json
 
-gmaps = googlemaps.Client(key='AIzaSyBMwXuzxKO2EY2eoNX163Iojydhmi39i1U') #From googlaMaps API
+''' gmaps = googlemaps.Client(key='AIzaSyBMwXuzxKO2EY2eoNX163Iojydhmi39i1U') #From googlaMaps API
 #print(dir(gmaps))
 gi = gmaps.distance_matrix('pasaje santa rosa 5040 buenos aires', 'Gascón 1099 buenos aires')
 #print(gi)
@@ -16,10 +16,20 @@ LAT2 = getInfo2[0]['geometry']['location']['lat']
 LONG2 = getInfo2[0]['geometry']['location']['lng']
 #print(LAT1, LONG1, 'AND', LAT2, LONG2)
 A = str('(') + str(LAT1) + str(',') + str(LONG1) + str(')')
-B = str('(') + str(LAT2) + str(',') + str(LONG2) + str(')')
-#INFO = osrm.simple_route([LAT1, LONG1],[LAT2, LONG2], output='route', overview="full", geometry='wkt')
-INFO = vincenty(A, B).kilometers
-#print(INFO)
+B = str('(') + str(LAT2) + str(',') + str(LONG2) + str(')') '''
+
+with open(os.path.join(os.path.dirname(__file__), 'latLongInfo.txt')) as X:
+    X = csv.reader(X)
+    for i, linei in enumerate(X):
+        with open(os.path.join(os.path.dirname(__file__), 'latLongInfo.txt')) as Y:
+            Y = csv.reader(Y)
+            for j, linej in enumerate(Y):
+                if i <= j:
+                    pass
+                else:
+                    distance = math.sqrt(pow(float(linei[2]) - float(linej[2]), 2)+ pow(float(linei[3]) - float(linej[3]), 2))
+                    print('This i:', linei[0], '/ This j:', linej[0], 'Distance:', distance)
+                    print('This j:', linej[0], '/ This i:', linei[0], 'Distance:', distance)
 
 response = urlopen('http://router.project-osrm.org/trip/v1/driving/-58.430085,-34.588136;-58.413186,-34.597955?source=first&destination=last')
 a = json.load(response)
