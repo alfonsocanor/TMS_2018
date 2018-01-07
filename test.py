741adc64f90d78fbd1916dc8d9db0f09ec43bf64
alfonsocano <lalfonsocanor@gmail.com>
2018-01-07 01:39:51 -0300

    Update_07012018
    Create csv with the iteration through the matrix directions - formula (m-1)*m/2

--- test.py
+++ test.py
@@ -24,21 +24,33 @@ listDistances = []
 with open(os.path.join(os.path.dirname(__file__), 'latLngInfocopy.csv')) as X:
     X = csv.reader(X)
     for i, linei in enumerate(X):
+        print(i)
         with open(os.path.join(os.path.dirname(__file__), 'latLngInfocopy.csv')) as Y:
             Y = csv.reader(Y)
             for j, linej in enumerate(Y):
+                print(j)
+                try:
+                    with open(os.path.join(os.path.dirname(__file__), 'distanceInfo')) as headersFile:
+                        pass
+                except FileNotFoundError:
+                    with open(os.path.join(os.path.dirname(__file__), 'distanceInfo'), 'a') as headersFile:
+                        headersFile = csv.writer(headersFile, delimiter = ',')
+                        headersFile.writerow(['id_AddressNumber_A', 'name_A', 'id_AddressNumber_B', 'name_B', 'distanceAB'])
                 if i <= j or linei[0]=='client_id' or linej[0]=='client_id':
                     pass
                 else:
-                    #print(linei[5],linei[4],linej[5],linei[4])
                     lookingForDistance = urlopen('http://router.project-osrm.org/trip/v1/driving/'+linei[5]+','+linei[4]+';'+linej[5]+','+linej[4]+'?source=first&destination=last')
                     infoFromOSRM = json.load(lookingForDistance)
                     distanceAB = infoFromOSRM['trips'][0]['legs'][0]['distance']
-                    info = 'From:',linei[0],linei[1],'to:',linej[0],linej[1],'The distance is:',distanceAB
-                    print('From:',linei[0],linei[1],'to:',linej[0],linej[1],'The distance is:',distanceAB)
-                    listDistances.append(info)
+                    with open(os.path.join(os.path.dirname(__file__), 'distanceInfo'), 'a') as distanceInfo:
+                        distanceInfo = csv.writer(distanceInfo, delimiter = ',')
+                        distanceInfo.writerow([str(linei[0]+'_'+linei[2]), linei[1], str(linej[0]+'_'+linej[2]), linej[1], str(distanceAB)])
+                        #print(linei[5],linei[4],linej[5],linei[4])
+                        #info = 'From:',linei[0],linei[1],'to:',linej[0],linej[1],'The distance is:',distanceAB
+                        #print('From:',linei[0],linei[1],'to:',linej[0],linej[1],'The distance is:',distanceAB)
+                        #listDistances.append(info)
 
-print(len(listDistances))
+#print(len(listDistances))
 
 
 
