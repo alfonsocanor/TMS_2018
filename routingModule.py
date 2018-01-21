54683a25b98b5006aa0d8cc053bf8a59d912ad2d
alfonsocano <lalfonsocanor@gmail.com>
2018-01-21 16:03:45 -0300

    update_21012018
    The web application shows a table order due the Dijkstra's algorithm

--- routingModule.py
+++ routingModule.py
@@ -1,12 +1,13 @@
 import csv
 import os
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect
 from flask_bootstrap import Bootstrap
 from time import strftime
 
 app = Flask(__name__)
 Bootstrap(app)
 app.config['SECRET_KEY'] = 'ThisissecretIFTS18'
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 class Route():
@@ -28,7 +29,6 @@ class Route():
             for row in routeSheetFile:
                 self.counterLine += 1
         while self.routes < self.counterLine:
-            print(self.routes, self.counterLine)
             with open(os.path.join(os.path.dirname(__file__), self.routeSheetFileName)) as routeSheetFile:
                 routeSheetFile = csv.reader(routeSheetFile)
                 for row_i in routeSheetFile:
@@ -55,70 +55,41 @@ class Route():
             self.startPoint = self.minDistance  
             self.distancesRouteDict = {}
             self.routes += 1
+        routingList = []
+        with open(os.path.join(os.path.dirname(__file__), self.routedFileName)) as routing_HTML_Table:
+            routing_HTML_Table = csv.reader(routing_HTML_Table)
+            for row in routing_HTML_Table:
+                routingList.append(row)
+        #routingList = [['0001_1954_1', 'AUTEIN SOCIEDAD ANONIMA', '1526.8']]
+        #print(routingList)
+        return routingList
 
-    '''def routing():
-        with open(os.path.join(os.path.dirname(__file__), 'routeSheet_12012018')) as routeSheetFile:
-            routeSheetFile = csv.reader(routeSheetFile)
-            for row in routeSheetFile:
-                counterLine += 1
+test = Route('routeSheet_12012018', 'totalDistances_AB_BA_12012018.csv' )
+print(test.routing1())
 
-        while routes < counterLine:
-            print(routes, counterLine)
-            with open(os.path.join(os.path.dirname(__file__), 'routeSheet_12012018')) as routeSheetFile:
-                routeSheetFile = csv.reader(routeSheetFile)
-                for row_i in routeSheetFile:
-                    id_Concatenate = startPoint + row_i[1]
-                    with open(os.path.join(os.path.dirname(__file__), 'totalDistances_AB_BA_12012018.csv')) as compareDistances:
-                        compareDistances = csv.reader(compareDistances)
-                        for row_j in compareDistances:
-                            #print(row_j[1], 'and', id_Concatenate)
-                            if (id_Concatenate == row_j[1]) and (row_j[4] not in distancesRouteList):
-                                #print('THE DICT VALUE TO ADD IS:', row_j)
-                                distancesRouteDict[row_j[4]] = float(row_j[6]) #It could be read: Client_id (row_j[4]) is far X meters (row_j[6]) from start point It's storaged in Dictionary (distancesRouteDict)
-                if distancesRouteDict == {}:
-                    pass
-                else:
-                    minDistance = min(distancesRouteDict, key=lambda k: distancesRouteDict[k]) #Formula found on internet for getting the min value in a dict
-                distancesRouteList.append(minDistance)
-            with open(os.path.join(os.path.dirname(__file__), 'totalDistances_AB_BA_12012018.csv')) as lookingUpClientInfo:
-                lookingUpClientInfo = csv.reader(lookingUpClientInfo)
-                for row_k in lookingUpClientInfo:
-                    if row_k[1] == startPoint + minDistance:
-                        with open(os.path.join(os.path.dirname(__file__), 'route_12012018.csv'), 'a') as routeFile:
-                            routeFile = csv.writer(routeFile, delimiter=',')
-                            routeFile.writerow([startPoint+row_k[4], row_k[5], row_k[6]])
-            startPoint = minDistance  
-            distancesRouteDict = {}
-            routes += 1'''
-
-X = Route('routeSheet_12012018', 'totalDistances_AB_BA_12012018.csv')
-X.routing1()
+@app.route('/home')
+def home():
+    return redirect('/routing')
 
 @app.route('/routing', methods=['GET', 'POST'])
 def routing():
+    #print(dir(request))
+    if request.method == 'POST':
+        #print('YES')
+        directory = os.path.join(APP_ROOT)
+        ifHTMLConditional = False
+        if not os.path.isdir(directory):
+            os.mkdir(directory)
+        for file in request.files.getlist("file"):
+            filename = file.filename
+            destination = "/".join([directory, filename])
+            file.save(destination)
+            objectFromClassRoute = Route(filename, 'totalDistances_AB_BA_12012018.csv')
+            returnFromObjectClassRoute = objectFromClassRoute.routing1()
+            ifHTMLConditional = True
+            return render_template('routing.html', returnFromObjectClassRoute=returnFromObjectClassRoute, ifHTMLConditional=ifHTMLConditional) 
+
     return render_template('routing.html')
 
-@app.route('/')
-def home():
-    return render_template('upload.html')
-
-@app.route('/routing', methods=['GET', 'POST'])
-def upload():
-    target = os.path.join(APP_ROOT)
-    print(target)
-
-    if not os.path.isdir(target):
-        os.mkdir(target)
-    
-    for file in request.files.getlist("file"):
-        print(dir(file))
-        filename = file.filename
-        print(filename)
-        destination = "/".join([target, filename])
-        print(destination)
-        file.save(destination)
-
-    return render_template('complete.html')
-
 if __name__=='__main__':
-    app.run(debug=False)
+    app.run(debug=True)
